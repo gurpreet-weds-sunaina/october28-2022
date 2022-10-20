1320e1bdb14f3e2945d4d064af9f36594cdc2845
Gurpreet Singh <gurpreet.singh6@telus.com>
2022-10-20 17:16:31 +0530

    Updates

---
+++ precache-manifest.fcd1272f8216fe1f2bab03029cb5e68a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db002c934dabdea352fafc83bde81a55",
+    "url": "/october28-2022/index.html"
+  },
+  {
+    "revision": "a13dd07524d82d9af3cb",
+    "url": "/october28-2022/static/js/2.3e0ff2e4.chunk.js"
+  },
+  {
+    "revision": "793a969fa0101dcb7ca9",
+    "url": "/october28-2022/static/js/main.af054aa6.chunk.js"
+  },
+  {
+    "revision": "0a5017d91d65109576bb",
+    "url": "/october28-2022/static/js/runtime-main.9efa3af4.js"
+  }
+]);
